053a29c4e259030f140a732e99f32027aa059bfc
Shafkathullah Ihsan <shafkathullah28@gmail.com>
2020-12-24 12:58:45 +0530

    control ui revamp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { RefreshIcon, PlayIcon, PauseIcon } from "./icons/index";
 import CircularProgress from "./components/CircularProgress";
+
 function App() {
   return (
     <div className="flex justify-center items-center h-full">
@@ -9,15 +10,14 @@ function App() {
           <button className="flex items-center px-4 py-1 mr-4 border border-white rounded hover:bg-gray-700 ">
             <PlayIcon classes="w-5 h-5 mr-1" />
             <span>Start</span>
+            {/* <PauseIcon classes="w-5 h-5 mr-1" />
+            <span>Pause</span> */}
           </button>
           <button className="flex items-center px-4 py-1 border border-white rounded hover:bg-gray-700 ">
-            <PauseIcon classes="w-5 h-5 mr-1" />
-            <span>Pause</span>
+            <RefreshIcon classes="w-5 h-5 mr-1" />
+            <span>Refresh</span>
           </button>
         </div>
-        <button className="text-center border rounded px-4 py-1 mt-4 hover:bg-gray-700 ">
-          <RefreshIcon classes="w-6 h-6" />
-        </button>
       </div>
     </div>
   );
