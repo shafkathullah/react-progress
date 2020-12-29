c02c6cc03e2c3cc5e9304e32922c13487cc5a712
Shafkathullah Ihsan <shafkathullah28@gmail.com>
2020-12-29 21:04:02 +0530

    completes requirments

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { RefreshIcon, PlayIcon, PauseIcon } from "./icons/index";
 import CircularProgress from "./components/CircularProgress";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="text-center">
-        <CircularProgress />
-        <div className="flex justify-center">
-          <button className="flex items-center px-4 py-1 mr-4 border border-white rounded hover:bg-gray-700 ">
-            <PlayIcon classes="w-5 h-5 mr-1" />
-            <span>Start</span>
-            {/* <PauseIcon classes="w-5 h-5 mr-1" />
-            <span>Pause</span> */}
-          </button>
-          <button className="flex items-center px-4 py-1 border border-white rounded hover:bg-gray-700 ">
-            <RefreshIcon classes="w-5 h-5 mr-1" />
-            <span>Refresh</span>
-          </button>
-        </div>
-      </div>
+    <div className="text-center">
+      <CircularProgress duration={2000} />
     </div>
   );
 }
